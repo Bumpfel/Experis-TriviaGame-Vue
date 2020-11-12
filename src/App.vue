84467e4e5c280930a00c225529f80b4f5d727193
Emil Oja <emil.oja@gmail.com>
2020-11-12 09:08:35 +0100

    chore: basic functions done

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,39 @@
 <template>
   <div id="app">
-    <p v-if="currentPollIndex<polls.length">{{ format(getNextPoll()) }}</p>
-    <p v-if="currentPollIndex == polls.length">Your score: {{score}}</p>
-    
-    <br />
 
-    <button v-for="(answer, index) of getAnswers(currentPollIndex)" :key="index" @click="isCorrect(answer)">
-      {{ format(answer) }}
-    </button>
-    <ul v-if="currentPollIndex == polls.length">
-  <li v-for="(item) in recordedAnswers" :key="item.question">
-  Question: {{item.question}} <br> Answer: {{ item.answer}} WasCorrect: {{item.wasCorrect}}
-  <br><br>
-  </li>
-</ul>
+    <button @click="started = true" v-if="!started">Start game</button>
+
+    <div v-if="started">
+      <p>Your score: {{ score }}</p>
+      <p v-if="currentPollIndex < polls.length">{{ format(getNextPoll()) }}</p>
+
+      <button v-for="(answer, index) of getAnswers(currentPollIndex)" :key="index" @click="isCorrect(answer)">{{ format(answer) }} </button>
+      
+      <ul v-if="currentPollIndex == polls.length">
+        <li v-for="(item, key) in recordedAnswers" :key="key">
+          Question: {{ format(key) }} <br />
+          Answer: {{ item.answer }} WasCorrect: {{ item.wasCorrect }} <br /><br />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script>
 
+<script>
 const shuffleArray = (array) => {
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
-}
+};
 
 export default {
   name: "App",
-  components: {},
+  components: {
+  },
   data() {
     fetch(
       "https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&encode=url3986"
@@ -39,45 +42,46 @@ export default {
     });
     return {
       polls: [],
-      answers: [],
       currentPollIndex: 0,
       recordedAnswers: {},
-      score: 0
+      score: 0,
+      started: false
     };
   },
   methods: {
+    print: function(msg) {
+      console.log(msg)
+    },
     getNextPoll: function () {
-      
-      if (this.polls.length > 0 && this.currentPollIndex < this.polls.length) {
+      if (this.polls.length > 0 ) {
         return this.polls[this.currentPollIndex].question;
       }
     },
     getAnswers: function () {
       if (this.polls.length > 0 && this.currentPollIndex < this.polls.length) {
-        const arr = []
-        this.polls[this.currentPollIndex].incorrect_answers.forEach(answer => arr.push(answer));
+        const arr = [];
+        this.polls[this.currentPollIndex].incorrect_answers.forEach((answer) =>
+          arr.push(answer)
+        );
         arr.push(this.polls[this.currentPollIndex].correct_answer);
 
-        shuffleArray(this.answers)
+        shuffleArray(arr);
 
-        return arr
+        return arr;
       }
     },
     isCorrect: function (answer) {
-      let correct = this.polls[this.currentPollIndex].correct_answer === answer
-      let question = this.polls[this.currentPollIndex].question
-      question = decodeURIComponent(question)
-      answer = decodeURIComponent(answer)
-      this.recordedAnswers[this.polls[this.currentPollIndex].question] = { question, answer, wasCorrect: correct }
-      
+      let correct = this.polls[this.currentPollIndex].correct_answer === answer;
+      answer = this.format(answer);
+      this.recordedAnswers[this.polls[this.currentPollIndex].question] = { answer, wasCorrect: correct };
       this.currentPollIndex++;
-      if(correct){
-        this.score++
+      if (correct) {
+        this.score += 10;
       }
     },
-    format: function(str) {
-      return decodeURIComponent(str)
-    }
+    format: function (str) {
+      return decodeURIComponent(str);
+    },
   },
 };
 </script>
